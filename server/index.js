a0066659c46e0f029c63b1ce936c607d4b1a6c8e
David Stanich <dstanich@gmail.com>
2017-02-12 23:06:24 -0600

    Update server code to have API to call robot

--- server/index.js
+++ server/index.js
@@ -1,7 +1,34 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 
+let piRover = require('./pi-rover');
+
 app.use(express.static('client'));
+app.use(bodyParser.json());
+
+// Respond to rover commands
+app.put('/rover', function (req, res) {
+	let direction = req.body.direction;
+	switch (direction) {
+		case 'forward':
+			piRover.forward();
+			break;
+		case 'left':
+			piRover.left();
+			break;
+		case 'right':
+			piRover.right();
+			break;
+		case 'backward':
+			piRover.backward();
+			break;
+		default:
+			break;
+	}
+
+	res.send('');
+});
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
---
+++ server/pi-rover.js
@@ -0,0 +1,27 @@
+function forward() {
+	console.log('FORWARD');
+}
+
+function left() {
+	console.log('LEFT');
+}
+
+function right() {
+	console.log('RIGHT');
+}
+
+function backward() {
+	console.log('BACKWARD');
+}
+
+function stop() {
+	console.log('STOP');
+}
+
+module.exports = {
+	forward,
+	left,
+	right,
+	backward,
+	stop
+};
